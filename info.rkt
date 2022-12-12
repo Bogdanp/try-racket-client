39624122efb1fdc9788ece335d56b6fe24c351e2
Bogdan Popa <bogdan@defn.io>
2022-12-12 10:25:13 +0200

    build: add license to info.rkt, fixup version

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 
-(define version "0.0")
+(define license 'BSD-3-Clause)
+(define version "0.1")
 (define collection "try-racket-client")
 (define deps '("base"))
 (define build-deps '())
